6086eb55cf5b057af7cf0ac519611a77fe187c66
nick <nickdenisov@protonmail.com>
2017-03-18 21:44:33 +0500

    Иисправлено(link_rx_speed):по умолчанию eth1

--- linlk_rx_speed.sh
+++ linlk_rx_speed.sh
@@ -2,12 +2,12 @@
 
 set -eu
 
-iface="$1"
+iface="${1:-eth1}"
 stat="/sys/class/net/${iface}/statistics/rx_bytes"
 prev=$(<$stat)
 while sleep 1; do
 	cur=$(<$stat)
-	echo "$cur $prev" | awk '{s=($1-$2)/1024; 
+	echo "$cur $prev" | awk '{s=($1-$2)/1024;
 	if(s>=1024){s=s/1024; printf "%.2f MB/s\n",s;}
 	else if(s/1024>=1024){s=s/1024; printf "%.2f GB/s\n",s;}
 	else{printf "%.2f KB/s\n",s}}'
