561a6b25b60180ff20673f56981a08fb21a41976
NikolayDenisov <nickdenisov@protonmail.com>
2017-03-18 19:06:24 +0500

    Update linlk_rx_speed.sh

--- linlk_rx_speed.sh
+++ linlk_rx_speed.sh
@@ -8,9 +8,8 @@ prev=$(<$stat)
 while sleep 1; do
 	cur=$(<$stat)
 	echo "$cur $prev" | awk '{s=($1-$2)/1024; 
-	if(s>=1){printf "%.2f KB/s\n",s;} 
-	else if(s>=1024){s=s/1024; printf "%.2f MB/s\n",s;}
-	else if(s/1024>=1){s=s/1024; printf "%.2f GB/s\n",s;}
+	if(s>=1024){s=s/1024; printf "%.2f MB/s\n",s;}
+	else if(s/1024>=1024){s=s/1024; printf "%.2f GB/s\n",s;}
 	else{printf "%.2f KB/s\n",s}}'
 	prev="$cur"
 done
