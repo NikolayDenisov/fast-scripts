bad4b07b43dacf162078bffa684ab64ee467a2a2
NikolayDenisov <nickdenisov@protonmail.com>
2017-03-21 10:10:21 +0500

    Update link_rx_speed.py

--- link_rx_speed.py
+++ link_rx_speed.py
@@ -3,20 +3,22 @@
 import sys
 import time
 
+
 def getbites():
     iface = "eth1" if len(sys.argv) < 2 else sys.argv[1]
     with open('/sys/class/net/{0}/statistics/rx_bytes'.format(iface)) as rx:
-        rx_bytes=rx.read()
+        rx_bytes = rx.read()
     return float(rx_bytes) * 8
+
 
 def humanize_rate(raw_speed):
     n = 2**10
-    if (raw_speed/n**2) >= n**2:
-        return "{0:.2f} Gbits/s".format(raw_speed/(n**3))
-    elif (raw_speed/n) >= n:
-        return "{0:.2f} Mbits/s".format(raw_speed/(n**2))
-    elif (raw_speed/n) >= 1:
-        return "{0:.2f} Kbits/s".format(raw_speed/n)
+    if (raw_speed / n**2) >= n**2:
+        return "{0:.2f} Gbits/s".format(raw_speed / (n**3))
+    elif (raw_speed / n) >= n:
+        return "{0:.2f} Mbits/s".format(raw_speed / (n**2))
+    elif (raw_speed / n) >= 1:
+        return "{0:.2f} Kbits/s".format(raw_speed / n)
     return "{0:.2f} bits/s".format(raw_speed)
 
 if __name__ == "__main__":
